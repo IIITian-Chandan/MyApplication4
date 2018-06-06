455aa4fd50db26b5187985f053905da0066a6972
OpenSource <cmishra646@gmail.com>
2018-06-07 05:21:13 +0530

    0.1 commit from chandan's pc

---
+++ app/src/main/java/com/example/opensource/myapplication/activities/home/Calender/Subject.java
@@ -0,0 +1,12 @@
+package com.example.opensource.myapplication.activities.home.Calender;
+
+public class Subject {
+
+    public String s_no;
+    public String machine_id;
+    public String machine_issue;
+    public String machine_location;
+    public String date;
+    public String accept_status;
+}
+
